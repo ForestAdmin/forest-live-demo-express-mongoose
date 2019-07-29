8dc91cd639ede5159ba42eadeb6e62f863ee2979
Steve Bunlon <bunlon.steve@gmail.com>
2019-07-29 10:53:16 +0200

    fix CORS issue

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
   origin: 'http://localhost:4200',
-  credential: true,
+  credentials: true,
 }));
 
 app.use('/', indexRouter);
